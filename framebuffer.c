681721451bcb47fbad58927e3bb797e0ddf0ae60
loidinhm31 <loidinh.git@gmail.com>
2022-12-18 12:49:28 +0700

    Build the serial ports for output

--- framebuffer.c
+++ framebuffer.c
@@ -8,10 +8,12 @@
 #define FB_HIGH_BYTE_COMMAND    14
 #define FB_LOW_BYTE_COMMAND     15
 
+#define FB_GREEN     2
+#define FB_DARK_GREY 8
+
 char *fb = (char *) 0x000B8000;
 
 int write(char *buf, unsigned int len);
-
 
 /** fb_write_cell:
      *  Writes a character with the given foreground and background to position i
@@ -37,4 +39,25 @@ void fb_move_cursor(unsigned short pos) {
     outb(FB_DATA_PORT, ((pos >> 8) & 0x00FF));
     outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
     outb(FB_DATA_PORT, pos & 0x00FF);
+}
+
+unsigned int* cursor_position() {
+    outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+    unsigned char high = inb(FB_DATA_PORT);
+
+    outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+    unsigned char low = inb(FB_DATA_PORT);
+
+    return (high << 8) | low;
+}
+
+int fb_write(char *buf, unsigned int len) {
+    unsigned int* position = cursor_position();
+
+    for (unsigned int i = 0; i < len; i++) {
+        fb_write_cell(position, *(buf + i), FB_GREEN, FB_DARK_GREY);
+        position += 1;
+        fb_move_cursor(position);
+    }
+    return 0;
 }
