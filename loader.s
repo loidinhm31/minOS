80e7e64b36073b9e0d2a16d60e9bf1152deb7bb7
loidinhm31 <loidinh.git@gmail.com>
2022-12-18 12:49:00 +0700

    Fix section for stack

--- loader.s
+++ loader.s
@@ -11,12 +11,16 @@ align 4                         ; the code must be 4 byte aligned
     dd MAGIC_NUMBER             ; write the magic number to the machine code,
     dd FLAGS                    ; the flags,
     dd CHECKSUM                 ; and the checksum
-loader:                         ; the loader label (defined as entry point in linker script)
-    mov esp, kernel_stack + KERNEL_STACK_SIZE ; point esp to the start of the
-.loop:
-    jmp .loop                   ; loop forever
 
 section .bss
 align 4                         ; align at 4 bytes
 kernel_stack:                   ; label points to beginning of memory
     resb KERNEL_STACK_SIZE      ; reserve stack for kernel
+
+loader:                         ; the loader label (defined as entry point in linker script)
+    mov esp, kernel_stack + KERNEL_STACK_SIZE ; point esp to the start of the
+
+.loop:
+    jmp .loop                   ; loop forever
+
+
