55fac8e51a512a24fdfdbcb997d57ef1ac0a4162
loidinhm31 <loidinh.git@gmail.com>
2022-12-18 14:48:51 +0700

    Change line space for comment code

--- io.s
+++ io.s
@@ -4,7 +4,6 @@ global outb             ; make the label outb visible outside this file
 ; stack: [esp + 8] the data byte
 ;        [esp + 4] the I/O port
 ;        [esp    ] return address
-
 _outb:
     mov al, [esp + 8]   ; move the data to be sent into the al register
     mov dx, [esp + 4]   ; move the address of the I/O port into the dx register
@@ -12,10 +11,10 @@ _outb:
     ret                 ; return to the calling function
 
 global inb
+
 ; inb - returns a byte from the given I/O port
 ; stack: [esp + 4] The address of the I/O port
 ;        [esp    ] The return address
-
 inb:
     mov dx, [esp + 4]   ; move the address of the I/O port to the dx register
     in  al, dx          ; read a byte from the I/O port and store it in the al register
